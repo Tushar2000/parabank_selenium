af4c12c464db81b37598bd3d2f2a28ec77f5cad2
Tushar2000 <tusharamar1@gmail.com>
2025-03-16 17:04:49 +0530

    second commit

---
+++ src/test/java/parabank/practicecommitfile.java
@@ -0,0 +1,13 @@
+package parabank;
+
+public class practicecommitfile {
+    public int addtwonumbers(int a , int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        practicecommitfile pcf = new practicecommitfile();
+        System.out.println(pcf.addtwonumbers(5,7));
+    }
+
+}
